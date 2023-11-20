340abc6619831814260e7d9c38122059ff73dbfe
Theeraporn <63072072+heroclik@users.noreply.github.com>
2023-11-20 23:32:59 +0700

    backend-initial commit

---
+++ server/configs/database.js
@@ -0,0 +1,12 @@
+const {Client} = require('pg')
+
+const client = new Client({
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    port: process.env.PGPORT,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE
+})
+
+
+module.exports = client
