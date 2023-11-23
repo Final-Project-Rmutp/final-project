e842d7d7cd72904cd8f399465468525f1b541d07
Theeraporn <63072072+heroclik@users.noreply.github.com>
2023-11-24 02:18:39 +0700

    Update database.js

--- server/configs/database.js
+++ server/configs/database.js
@@ -7,7 +7,7 @@ const client = new Client({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     port: process.env.DB_PORT,
-    port: process.env.DB__PORT,
+    port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     ssl: {
