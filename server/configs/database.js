a3ac61d83a1524c5415ec7359f4b188d401218c8
Theeraporn <63072072+heroclik@users.noreply.github.com>
2023-11-24 02:01:54 +0700

    edit database

--- server/configs/database.js
+++ server/configs/database.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const client = new Client({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    port: process.env.PORT,
+    port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     ssl: {
