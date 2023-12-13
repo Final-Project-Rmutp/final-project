5e8c2ca7a1e9c62a15f58f97180fc4292f52db8a
Toon <wisit.m@unigainfo.com>
2023-12-14 01:59:08 +0700

    fix : bug route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,8 +17,7 @@ import { Toaster} from 'sonner'
 // import CssBaseline from '@mui/material/CssBaseline';
 import User from "./pages/student/student";
 
-import { CssVarsProvider } from '@mui/joy/styles';
-import CssBaseline from '@mui/joy/CssBaseline';
+
 ////admin
 import StudentList from "./pages/admin/student-list/StudentList";
 import ReservedList from "./pages/admin/reserved-list/ReservedList";
@@ -28,45 +27,45 @@ import RoomList from "./pages/admin/room-list/RoomList";
 const App: React.FC = () => {
 
   return (
-    <CssVarsProvider>
-    <CssBaseline />
-        <Router>
-              <AuthProvider>
-                <Routes>
-                  <Route
-                    path="/"
-                    element={
-                      <UnAuthenticatedRoute>
-                        <Login />
-                      </UnAuthenticatedRoute>
-                    }
-                  />
 
-                  <Route
-                    path="/user"
-                    element={
-                      <AuthenticatedRoute>
-                        <User />
-                      </AuthenticatedRoute>
-                    }
-                  />
-                    <Route
-                      path="admin"
-                      element={
-                        <AuthenticatedRoute>
-                              <AdminProfileSidebar></AdminProfileSidebar>
-                        </AuthenticatedRoute>
-                      }
-                    >
-                      <Route path="student-list" element={<StudentList />} />
-                      <Route path="reserved-list" element={<ReservedList />} />
-                      <Route path="room-list" element={<RoomList />} />
-                    </Route>
-                </Routes>
-              </AuthProvider>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <UnAuthenticatedRoute>
+                    <Login />
+                  </UnAuthenticatedRoute>
+                }
+              />
+
+              <Route
+                path="/user"
+                element={
+                  <AuthenticatedRoute>
+                    <User />
+                  </AuthenticatedRoute>
+                }
+              />
+
+              <Route
+                path="admin"
+                element={
+                  <AuthenticatedRoute>
+                    <AdminProfileSidebar></AdminProfileSidebar>
+                  </AuthenticatedRoute>
+                }
+              >
+                <Route path="student-list" element={<StudentList />} />
+                <Route path="reserved-list" element={<ReservedList />} />
+                <Route path="room-list" element={<RoomList />} />
+              </Route>
+            </Routes>
+          </AuthProvider>
           <Toaster  richColors expand={true} />
         </Router>
-    </CssVarsProvider>
+
   );
 };
 
