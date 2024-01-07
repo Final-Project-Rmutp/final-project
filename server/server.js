fb0f15e9cafcc6020a0cd9f19075929c0469e208
Toon <wisit.m@unigainfo.com>
2024-01-07 23:00:54 +0700

    asd

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads");
+    cb(null, "../uploads");
   },
   filename: function (req, file, cb) {
     cb(null, `${Date.now()}-${file.originalname}`);
