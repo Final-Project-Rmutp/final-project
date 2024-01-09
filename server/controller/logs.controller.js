02a6046f08a4b22b6f98eb2ed50a994c472dc68f
Theeraporn <63072072+heroclik@users.noreply.github.com>
2024-01-10 06:07:33 +0700

    Update logs

--- server/controller/logs.controller.js
+++ server/controller/logs.controller.js
@@ -6,9 +6,9 @@ async function getlogs(req, res) {
         let query = `SELECT logs.logs_id, action_type.action_type_name, "user".pin, "user".citizen_id, "user".firstname, logs.action_status, logs.logs_message, logs.timestamp
         FROM action_logs AS logs
         LEFT JOIN "user" ON logs.user_id = "user".id
-		LEFT JOIN action_type on logs.action_type_id = action_type.action_type_id`;
+        LEFT JOIN action_type ON logs.action_type_id = action_type.action_type_id`;
 
-        const { startDate, endDate } = req.query;
+        const { startDate, endDate, action_type_name, action_status } = req.query;
         const values = [];
 
         if (startDate && endDate) {
@@ -18,32 +18,24 @@ async function getlogs(req, res) {
             values.push(startDateTime, endDateTime);
         }
 
-        const result = await client.query(query, values);
-        const totalLogs = result.rowCount;
-        res.status(200).json({ totalLogs, logs: result.rows });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-}
+        if (action_type_name) {
+            if (values.length === 0) {
+                query += ` WHERE`;
+            } else {
+                query += ` AND`;
+            }
+            query += ` action_type.action_type_name = $${values.length + 1}`;
+            values.push(action_type_name);
+        }
 
-// Get login logs
-async function loginlogs(req, res) {
-    try {
-        let query = `
-            SELECT logs.log_id, logs.action_type, "user".pin, "user".citizen_id, "user".firstname, logs.log_status, logs.timestamp
-            FROM action_logs AS logs
-            LEFT JOIN "user" ON logs.user_id = "user".id
-            WHERE logs.action_type ILIKE '%login%'`;
-
-        const { startDate, endDate } = req.query;
-        const values = [];
-
-        if (startDate && endDate) {
-            const startDateTime = `${startDate} 00:00:00`;
-            const endDateTime = `${endDate} 23:59:59`;
-            query += ` AND logs.timestamp >= $1 AND logs.timestamp <= $2`;
-            values.push(startDateTime, endDateTime);
+        if (action_status) {
+            if (values.length === 0) {
+                query += ` WHERE`;
+            } else {
+                query += ` AND`;
+            }
+            query += ` logs.action_status = $${values.length + 1}`;
+            values.push(action_status);
         }
 
         const result = await client.query(query, values);
@@ -55,172 +47,4 @@ async function loginlogs(req, res) {
     }
 }
 
-// Get adduser logs
-async function adduserlogs(req, res) {
-    try {
-        let query = `SELECT logs.log_id, logs.action_type, "user".pin, "user".citizen_id, "user".firstname, logs.log_status, logs.timestamp
-        FROM action_logs AS logs
-        LEFT JOIN "user" ON logs.user_id = "user".id
-        WHERE logs.action_type like '%adduser%'
-        `;
-
-        const { startDate, endDate } = req.query;
-        const values = [];
-
-        if (startDate && endDate) {
-            const startDateTime = `${startDate} 00:00:00`;
-            const endDateTime = `${endDate} 23:59:59`;
-            query += ` AND logs.timestamp >= $1 AND logs.timestamp <= $2`;
-            values.push(startDateTime, endDateTime);
-        }
-
-        const result = await client.query(query, values);
-        const totalLogs = result.rowCount;
-        res.status(200).json({ totalLogs, logs: result.rows });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-}
-
-// Get updateuser logs
-async function updateuserlogs(req, res) {
-    try {
-        let query = `SELECT logs.log_id, logs.action_type, "user".pin, "user".citizen_id, "user".firstname, logs.log_status, logs.timestamp
-        FROM action_logs AS logs
-        LEFT JOIN "user" ON logs.user_id = "user".id
-        WHERE logs.action_type like '%update%'
-        `;
-
-        const { startDate, endDate } = req.query;
-        const values = [];
-
-        if (startDate && endDate) {
-            const startDateTime = `${startDate} 00:00:00`;
-            const endDateTime = `${endDate} 23:59:59`;
-            query += ` AND logs.timestamp >= $1 AND logs.timestamp <= $2`;
-            values.push(startDateTime, endDateTime);
-        }
-
-        const result = await client.query(query, values);
-        const totalLogs = result.rowCount;
-        res.status(200).json({ totalLogs, logs: result.rows });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-}
-
-// Get deactivate logs
-async function deactivatelogs(req, res) {
-    try {
-        let query = `SELECT logs.log_id, logs.action_type, "user".pin, "user".citizen_id, "user".firstname, logs.log_status, logs.timestamp
-        FROM action_logs AS logs
-        LEFT JOIN "user" ON logs.user_id = "user".id
-        WHERE logs.action_type like '%deactivate%'
-        `;
-
-        const { startDate, endDate } = req.query;
-        const values = [];
-
-        if (startDate && endDate) {
-            const startDateTime = `${startDate} 00:00:00`;
-            const endDateTime = `${endDate} 23:59:59`;
-            query += ` AND logs.timestamp >= $1 AND logs.timestamp <= $2`;
-            values.push(startDateTime, endDateTime);
-        }
-
-        const result = await client.query(query, values);
-        const totalLogs = result.rowCount;
-        res.status(200).json({ totalLogs, logs: result.rows });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-}
-
-// Get addroom logs
-async function addroomlogs(req, res) {
-    try {
-        let query = `SELECT logs.log_id, logs.action_type, "user".pin, "user".citizen_id, "user".firstname, logs.log_status, logs.timestamp
-        FROM action_logs AS logs
-        LEFT JOIN "user" ON logs.user_id = "user".id
-        WHERE logs.action_type like '%addroom%'
-        `;
-
-        const { startDate, endDate } = req.query;
-        const values = [];
-
-        if (startDate && endDate) {
-            const startDateTime = `${startDate} 00:00:00`;
-            const endDateTime = `${endDate} 23:59:59`;
-            query += ` AND logs.timestamp >= $1 AND logs.timestamp <= $2`;
-            values.push(startDateTime, endDateTime);
-        }
-
-        const result = await client.query(query, values);
-        const totalLogs = result.rowCount;
-        res.status(200).json({ totalLogs, logs: result.rows });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-}
-
-// Get updateroom logs
-async function updateroomlogs(req, res) {
-    try {
-        let query = `SELECT logs.log_id, logs.action_type, "user".pin, "user".citizen_id, "user".firstname, logs.log_status, logs.timestamp
-        FROM action_logs AS logs
-        LEFT JOIN "user" ON logs.user_id = "user".id
-        WHERE logs.action_type like '%updateroom%'
-        `;
-
-        const { startDate, endDate } = req.query;
-        const values = [];
-
-        if (startDate && endDate) {
-            const startDateTime = `${startDate} 00:00:00`;
-            const endDateTime = `${endDate} 23:59:59`;
-            query += ` AND logs.timestamp >= $1 AND logs.timestamp <= $2`;
-            values.push(startDateTime, endDateTime);
-        }
-
-        const result = await client.query(query, values);
-        const totalLogs = result.rowCount;
-        res.status(200).json({ totalLogs, logs: result.rows });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-}
-
-// Get deleteroom logs
-async function deleteroomlogs(req, res) {
-    try {
-        let query = `SELECT logs.log_id, logs.action_type, "user".pin, "user".citizen_id, "user".firstname, logs.log_status, logs.timestamp
-        FROM action_logs AS logs
-        LEFT JOIN "user" ON logs.user_id = "user".id
-        WHERE logs.action_type like '%deleteroom%'
-        `;
-
-        const { startDate, endDate } = req.query;
-        const values = [];
-
-        if (startDate && endDate) {
-            const startDateTime = `${startDate} 00:00:00`;
-            const endDateTime = `${endDate} 23:59:59`;
-            query += ` AND logs.timestamp >= $1 AND logs.timestamp <= $2`;
-            values.push(startDateTime, endDateTime);
-        }
-
-        const result = await client.query(query, values);
-        const totalLogs = result.rowCount;
-        res.status(200).json({ totalLogs, logs: result.rows });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-}
-
-module.exports = { getlogs, loginlogs, adduserlogs, updateuserlogs, deactivatelogs, addroomlogs, updateroomlogs, deleteroomlogs };
+module.exports = { getlogs };
