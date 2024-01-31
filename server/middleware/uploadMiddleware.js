90242ee71bd80d6833083ff0ceffe4681ac3c91e
Theeraporn <63072072+heroclik@users.noreply.github.com>
2024-01-31 19:20:40 +0700

    Update uploadMiddleware.js

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,4 +1,4 @@
-const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const { S3Client, PutObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
 const multer = require('multer');
 const sharp = require('sharp');
 const fileType = require('file-type');
@@ -18,118 +18,64 @@ const upload = multer();
 // Middleware function for S3 upload
 const uploadToS3 = upload.single('image');
 
-// Handle S3 upload
 const handleS3Upload = async (req, res, next) => {
   const file = req.file;
-  if (!file) {
-    return res.status(400).json({ error: 'No file uploaded' });
-  }
 
-  const buffer = file.buffer; // Assuming multer stores the file buffer in req.file.buffer
-  const fileTypeResult = fileType(buffer);
+  // Check if a file was uploaded
+  if (file) {
+    const buffer = file.buffer; // Assuming multer stores the file buffer in req.file.buffer
+    const fileTypeResult = fileType(buffer);
 
-  // Check if the uploaded file is an image
-  if (!fileTypeResult || !fileTypeResult.mime.startsWith('image')) {
-    return res.status(400).json({ error: 'Uploaded file is not an image' });
-  }
-
-  try {
-    const pin = req.body.pin;
-    const fileName = pin + '.webp';
-
-    // Check if the file already exists in S3
-    const headParams = {
-      Bucket: process.env.S3_BUCKET,
-      Key: fileName
-    };
-
-    try {
-      // Check if the file exists
-      await s3Client.send(new HeadObjectCommand(headParams));
-
-      // If it exists, add a timestamp to the file name
-      const timestamp = new Date().getTime();
-      fileName = pin + `_${timestamp}.webp`;
-    } catch (error) {
-      // File doesn't exist, proceed with the original file name
+    // Check if the uploaded file is an image
+    if (!fileTypeResult || !fileTypeResult.mime.startsWith('image')) {
+      return res.status(400).json({ error: 'Uploaded file is not an image' });
     }
 
-    // Convert image to WebP format using sharp
-    const webpBuffer = await sharp(buffer).toFormat('webp').toBuffer();
-
-    const uploadParams = {
-      Bucket: process.env.S3_BUCKET,
-      Key: fileName,
-      Body: webpBuffer,
-      ContentType: 'image/webp'
-    };
-
-    const uploadCommand = new PutObjectCommand(uploadParams);
-    const uploadResult = await s3Client.send(uploadCommand);
-
-    req.uploadedFileUrl = `https://${process.env.S3_BUCKET}.s3.${process.env.S3_REGION}.amazonaws.com/${fileName}`;
-    next();
-  } catch (error) {
-    console.error('Error uploading file:', error);
-    return res.status(500).json({ error: 'Failed to upload file' });
-  }
-};
-
-// Handle S3 update
-const handleS3Update = async (req, res, next) => {
-  const file = req.file;
-  if (!file) {
-    return res.status(400).json({ error: 'No file uploaded' });
-  }
-
-  const buffer = file.buffer; // Assuming multer stores the file buffer in req.file.buffer
-  const fileTypeResult = fileType(buffer);
-
-  // Check if the uploaded file is an image
-  if (!fileTypeResult || !fileTypeResult.mime.startsWith('image')) {
-    return res.status(400).json({ error: 'Uploaded file is not an image' });
-  }
-
-  try {
-    const pin = req.body.pin;
-    const fileName = pin + '.webp';
-
-    // Check if the file already exists in S3
-    const headParams = {
-      Bucket: process.env.S3_BUCKET,
-      Key: fileName
-    };
-
     try {
-      // Check if the file exists
-      await s3Client.send(new HeadObjectCommand(headParams));
+      const pin = req.body.pin;
+      let fileName = pin + '.webp';
 
-      // If it exists, add a timestamp to the file name
-      const timestamp = new Date().getTime();
-      fileName = pin + `_${timestamp}.webp`;
+      // Check if the file already exists in S3
+      const headParams = {
+        Bucket: process.env.S3_BUCKET,
+        Key: fileName
+      };
+
+      try {
+        // Check if the file exists
+        await s3Client.send(new HeadObjectCommand(headParams));
+
+        // If it exists, add a timestamp to the file name
+        const timestamp = new Date().getTime();
+        fileName = pin + `_${timestamp}.webp`;
+      } catch (error) {
+        // File doesn't exist, proceed with the original file name
+      }
+
+      // Convert image to WebP format using sharp
+      const webpBuffer = await sharp(buffer).toFormat('webp').toBuffer();
+
+      const uploadParams = {
+        Bucket: process.env.S3_BUCKET,
+        Key: fileName,
+        Body: webpBuffer,
+        ContentType: 'image/webp'
+      };
+
+      const uploadCommand = new PutObjectCommand(uploadParams);
+      const uploadResult = await s3Client.send(uploadCommand);
+
+      req.uploadedFileUrl = `https://${process.env.S3_BUCKET}.s3.${process.env.S3_REGION}.amazonaws.com/${fileName}`;
     } catch (error) {
-      // File doesn't exist, proceed with the original file name
+      console.error('Error uploading file:', error);
+      return res.status(500).json({ error: 'Failed to upload file' });
     }
-
-    // Convert image to WebP format using sharp
-    const webpBuffer = await sharp(buffer).toFormat('webp').toBuffer();
-
-    const uploadParams = {
-      Bucket: process.env.S3_BUCKET,
-      Key: fileName,
-      Body: webpBuffer,
-      ContentType: 'image/webp'
-    };
-
-    const uploadCommand = new PutObjectCommand(uploadParams);
-    const uploadResult = await s3Client.send(uploadCommand);
-
-    req.uploadedFileUrl = `https://${process.env.S3_BUCKET}.s3.${process.env.S3_REGION}.amazonaws.com/${fileName}`;
-    next();
-  } catch (error) {
-    console.error('Error uploading file:', error);
-    return res.status(500).json({ error: 'Failed to upload file' });
+  } else {
+    // No file was uploaded
+    req.uploadedFileUrl = null;
   }
+
+  next();
 };
 
 // Function to delete an object from S3
@@ -150,7 +96,7 @@ async function deleteObjectFromS3(objectKey) {
 
     const deleteCommand = new DeleteObjectCommand(deleteParams);
     await s3Client.send(deleteCommand);
-    console.log(`Object ${objectKey} deleted successfully from S3`);
+    // console.log(`Object ${objectKey} deleted successfully from S3`);
   } catch (error) {
     console.error('Error deleting object from S3:', error);
     throw error;
